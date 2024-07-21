1d458e404b90733c913afb002bfbe047b23b45c2
ice <ice@xicex.fr>
2024-07-21 19:22:19 +0200

    Adjust responsive

--- app/components/stack/StackList.tsx
+++ app/components/stack/StackList.tsx
@@ -2,7 +2,7 @@ import IconStack from "./IconStack";
 
 const StackList = () => {
     return (
-        <div className="flex flex-wrap justify-center sm:grid grid-cols-6 grid-rows-2 gap-3 lg:gap-6 lg:py-16 xl:py-24 ">
+        <div className="gap-1 grid grid-cols-6 grid-rows-2 lg:gap-6 lg:py-16 xl:py-24 ">
             <IconStack stackName="HTML" iconName="devicon:html5" />
             <IconStack stackName="CSS" iconName="devicon:css3" />
             <IconStack stackName="Tailwind" iconName="logos:tailwindcss-icon" />
