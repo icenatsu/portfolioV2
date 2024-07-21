261eec51493140b3ea0df750ca4b3487771f3065
ice <ice@xicex.fr>
2024-07-21 21:23:28 +0200

    Adjustement links NavLink Burger

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -13,18 +13,18 @@ const NavLinks = ({ className }: NavLinksProps) => {
 
     return (
         <ul className={twMerge(className, "navbar-text")}>
-            <li className="hover:bg-cyan-700 p-1 lg:p-6 lg:hover:underline lg:hover:bg-transparent dark:lg:hover:bg-transparent">
-                <Link href="/#home">{languageContext?.isFrenchLanguage ? "Accueil" : "Home"}</Link>
+            <li className="hover:bg-cyan-700 px-2 py-1 lg:p-6 lg:hover:underline lg:hover:bg-transparent dark:lg:hover:bg-transparent">
+                <Link className="block w-full" href="/#home">{languageContext?.isFrenchLanguage ? "Accueil" : "Home"}</Link>
             </li>
-            <li className="hover:bg-cyan-700 p-1 lg:p-6 lg:hover:underline lg:hover:bg-transparent dark:lg:hover:bg-transparent">
-                <Link href="/#about">{languageContext?.isFrenchLanguage ? <>&Agrave; propos</> : "About"}</Link>
+            <li className="hover:bg-cyan-700 px-2 py-1 lg:p-6 lg:hover:underline lg:hover:bg-transparent dark:lg:hover:bg-transparent">
+                <Link className="block w-full" href="/#about">{languageContext?.isFrenchLanguage ? <>&Agrave; propos</> : "About"}</Link>
             </li>
-            <li className="hover:bg-cyan-700 p-1 lg:p-6 lg:hover:underline lg:hover:bg-transparent dark:lg:hover:bg-transparent">
-                <Link href="/#stack">Tech stack</Link></li>
-            <li className="hover:bg-cyan-700 p-1 lg:p-6 lg:hover:underline lg:hover:bg-transparent dark:lg:hover:bg-transparent">
-                <Link href="/#project">{languageContext?.isFrenchLanguage ? "Projets" : "Projects"}</Link></li>
-            <li className="hover:bg-cyan-700 p-1 lg:p-6 lg:hover:underline lg:hover:bg-transparent dark:lg:hover:bg-transparent">
-                <Link href="/#contact">Contact</Link>
+            <li className="hover:bg-cyan-700 px-2 py-1 lg:p-6 lg:hover:underline lg:hover:bg-transparent dark:lg:hover:bg-transparent">
+                <Link className="block w-full"href="/#stack">Tech stack</Link></li>
+            <li className="hover:bg-cyan-700 px-2 py-1 lg:p-6 lg:hover:underline lg:hover:bg-transparent dark:lg:hover:bg-transparent">
+                <Link className="block w-full" href="/#project">{languageContext?.isFrenchLanguage ? "Projets" : "Projects"}</Link></li>
+            <li className="hover:bg-cyan-700 px-2 py-1 lg:p-6 lg:hover:underline lg:hover:bg-transparent dark:lg:hover:bg-transparent">
+                <Link className="block w-full" href="/#contact">Contact</Link>
             </li>
         </ul>
     );
